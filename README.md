7d5367da88d5bbbbe08dee26fe26a257fcca68fd
Alison Gale <agale753@gmail.com>
2023-02-28 21:31:20 -0800

    Update README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# nwslpy
+# nwslpy <img src='man/figures/nwslR.png' align="right" height="139" />
 
 `nwslpy` is a Python wrapper around the [nwslR](https://github.com/nwslR/nwslR) package. The goal of this package is to make the data from `nwslR` available to even more people.
 
@@ -6,15 +6,67 @@ If you see anything you’d like added, changed, or updated, please open up a ne
 
 ## Installing the package
 
-To install the package, run
+This package requires an R installation with the `devtools` library installed.
+
+### Temporary development steps
+
+First you will need to clone this repository and switch into that directory:
+
+```
+git clone https://github.com/agale123/nwslpy.git
+cd nwslpy
+```
+
+To install the package, run:
 
 ```
 python setup.py install
 ```
 
+## API
+
+- `load_player_match_stats(match_id)`: Loads player level stats for a given
+  match
+- `load_player_season_stats(team_id, season)`: Loads player level stats for a
+  team/season
+- `load_team_match_stats(match_id)`: Loads team level stats for a given match
+- `load_team_season_stats(team_id, season)`: Loads team level stats for a team/season
+- `load_matches()`: All matches from 2016-present with information and
+  match IDs
+- `load_players()`: All players rostered from 2016-present with
+  information and player IDs
+- `load_teams()`: All teams active from 2016-present with information
+  and team IDs
+- `load_metrics()` All metrics available from scrapers with definitions.
+  Not all metrics are available for all players/matches/teams/etc.
+
 ## Usage
 
-TODO
+If you wanted to see the list of matches that were won on penalties, you could try:
+
+```py
+import nwslpy
+
+# Load all matches and filter to ones in the year 2022
+matches = nwslpy.load_matches()
+matches[matches["won_on_penalties"]]
+```
+
+If you wanted to see which players took the most shots in the 2022 NWSL Championship, you could try 
+
+```py
+import nwslpy
+
+stats = nwslpy.load_player_match_stats("portland-thorns-fc-vs-kansas-city-current-2022-10-29")
+players = nwslpy.load_players()
+
+stats[["player_id", "shots_total"]].groupby("player_id").sum("shots_total").join(
+    players.set_index("player_id")
+)[["shots_total", "player_match_name"]].sort_values("shots_total", ascending=False)
+```
+
+For more complicated examples, including how to visualize the data, check out the [examples](examples/) directory.
+
 
 ## Package structure
 
@@ -23,7 +75,7 @@ nwslpy
 ├── LICENSE
 ├── README.md
 ├── examples
-│   └── simple.py
+│   └── example_1.ipynb
 ├── nwslpy
 │   ├── __init__.py
 │   └── nwslpy.py
--- nwslpy/nwslpy.py
+++ nwslpy/nwslpy.py
@@ -4,11 +4,11 @@ from rpy2.robjects import pandas2ri
 import rpy2.rinterface_lib.callbacks
 import pandas as pd
 
-# Redirect R output
+# Redirect R output since this prints a lot of messages
 rpy2.rinterface_lib.callbacks.consolewrite_print = lambda *args: None
 rpy2.rinterface_lib.callbacks.consolewrite_warnerror = lambda *args: None
 
-# Initialization needed for all requests
+# Use devtools to install the nwslR package from GitHub
 utils = rpackages.importr("utils")
 utils.install_packages("devtools")
 devtools = rpackages.importr("devtools")
@@ -40,6 +40,7 @@ def _convert_and_fix_types(res):
 
 
 def _run_query(method, args={}):
+    """Run the specified method (with optional args) in R."""
 
     return robjects.r(
         """
@@ -49,44 +50,58 @@ def _run_query(method, args={}):
         """.format(
             method=method,
             formatted_args=",".join(
-                list(map(lambda x: "'" + x[0] + "'='" + x[1] + "'", args.items())))
+                map(lambda x: "'" + x[0] + "'='" + x[1] + "'", args.items())
+            ),
         )
     )
 
 
 def load_matches():
+    """All matches from 2016-present with information and match IDs."""
     return _convert_and_fix_types(_run_query("load_matches"))
 
 
 def load_players():
+    """All players rostered from 2016-present with information and player IDs."""
     return _convert_and_fix_types(_run_query("load_players"))
 
 
 def load_teams():
+    """All teams active from 2016-present with information and team IDs"""
     return _convert_and_fix_types(_run_query("load_teams"))
 
 
 def load_metrics():
+    """All metrics available from scrapers with definitions.
+
+    Not all metrics are available for all players/matches/teams/etc.
+    """
     return _convert_and_fix_types(_run_query("load_metrics"))
 
 
 def load_team_season_stats(team_id, season):
+    """Loads team level stats for a team/season."""
     return _convert_and_fix_types(
-        _run_query("load_team_season_stats", {
-                   "team_id": team_id, "season": season})
+        _run_query("load_team_season_stats", {"team_id": team_id, "season": season})
     )
 
 
 def load_player_season_stats(team_id, season):
+    """Loads player level stats for a team/season."""
     return _convert_and_fix_types(
-        _run_query("load_player_season_stats", {
-                   "team_id": team_id, "season": season})
+        _run_query("load_player_season_stats", {"team_id": team_id, "season": season})
     )
 
 
 def load_team_match_stats(match_id):
-    return _convert_and_fix_types(_run_query("load_team_match_stats", {"match_id": match_id}))
+    """Loads team level stats for a given match."""
+    return _convert_and_fix_types(
+        _run_query("load_team_match_stats", {"match_id": match_id})
+    )
 
 
 def load_player_match_stats(match_id):
-    return _convert_and_fix_types(_run_query("load_player_match_stats", {"match_id": match_id}))
+    """Loads player level stats for a given match."""
+    return _convert_and_fix_types(
+        _run_query("load_player_match_stats", {"match_id": match_id})
+    )
