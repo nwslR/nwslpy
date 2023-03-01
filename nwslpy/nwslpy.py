04c447147019647ab81c8b91192f32501993d0b9
Alison Gale <agale753@gmail.com>
2023-03-01 08:30:25 -0800

    Update to read directly from GitHub

--- nwslpy/nwslpy.py
+++ nwslpy/nwslpy.py
@@ -1,74 +1,51 @@
-import rpy2.robjects.packages as rpackages
-import rpy2.robjects as robjects
-from rpy2.robjects import pandas2ri
-import rpy2.rinterface_lib.callbacks
 import pandas as pd
 
-# Redirect R output since this prints a lot of messages
-rpy2.rinterface_lib.callbacks.consolewrite_print = lambda *args: None
-rpy2.rinterface_lib.callbacks.consolewrite_warnerror = lambda *args: None
-
-# Use devtools to install the nwslR package from GitHub
-utils = rpackages.importr("utils")
-utils.install_packages("devtools")
-devtools = rpackages.importr("devtools")
-devtools.install_github("nwslR/nwslR")
-rpackages.importr("nwslR")
+TYPES = {"kickoff": "datetime", "time": "time", "last_updated": "date"}
 
 
-def _convert_and_fix_types(res):
-    """Converts the raw output from rpy2 into Python types."""
+def _fix_types(df):
+    """Coerce types for certain columns."""
+    for col in df.columns:
+        if col in TYPES.keys():
+            t = TYPES[col]
+            if t == "datetime":
+                df[col] = pd.to_datetime(df[col])
+            elif t == "date":
+                df[col] = pd.to_datetime(df[col]).dt.date
+            elif t == "time":
+                df[col] = pd.to_datetime(df[col]).dt.time
 
-    df = pandas2ri.rpy2py(res[0])
-    types = (
-        pandas2ri.rpy2py(res[1])
-        if type(res[1]) == robjects.vectors.StrVector
-        else [pandas2ri.rpy2py(x)[0] for x in res[1]]
-    )
-    for i, (col, t) in enumerate(zip(df.columns, types)):
-        if "_pct" in col or "_accuracy" in col:
-            df[col] = df[col].astype(float)
-        elif t == "numeric":
-            df[col] = df[col].astype("Int64")
-        elif t == "logical":
-            df[col] = df[col].astype(bool)
-        elif t == "hms":
-            df[col] = pd.to_datetime(df[col], unit="s").dt.time
-        elif t == "Date":
-            df[col] = pd.to_datetime(df[col], unit="d")
     return df
-
-
-def _run_query(method, args={}):
-    """Run the specified method (with optional args) in R."""
-
-    return robjects.r(
-        """
-        data <- {method}({formatted_args})
-        types <- sapply(data, class)
-        list(data, types)
-        """.format(
-            method=method,
-            formatted_args=",".join(
-                map(lambda x: "'" + x[0] + "'='" + x[1] + "'", args.items())
-            ),
-        )
-    )
 
 
 def load_matches():
     """All matches from 2016-present with information and match IDs."""
-    return _convert_and_fix_types(_run_query("load_matches"))
+    return _fix_types(
+        pd.read_csv(
+            "https://github.com/nwslR/nwsldata/releases/download/key_tables/matches.csv",
+            index_col=0,
+        )
+    )
 
 
 def load_players():
     """All players rostered from 2016-present with information and player IDs."""
-    return _convert_and_fix_types(_run_query("load_players"))
+    return _fix_types(
+        pd.read_csv(
+            "https://github.com/nwslR/nwsldata/releases/download/key_tables/players.csv",
+            index_col=0,
+        )
+    )
 
 
 def load_teams():
     """All teams active from 2016-present with information and team IDs"""
-    return _convert_and_fix_types(_run_query("load_teams"))
+    return _fix_types(
+        pd.read_csv(
+            "https://github.com/nwslR/nwsldata/releases/download/key_tables/teams.csv",
+            index_col=0,
+        )
+    )
 
 
 def load_metrics():
@@ -76,32 +53,87 @@ def load_metrics():
 
     Not all metrics are available for all players/matches/teams/etc.
     """
-    return _convert_and_fix_types(_run_query("load_metrics"))
+    return _fix_types(
+        pd.read_csv(
+            "https://github.com/nwslR/nwsldata/releases/download/key_tables/metrics.csv",
+            index_col=0,
+        )
+    )
+
+
+available_teams = list(load_teams()["team_abbreviation"])
+available_seasons = list(load_matches()["season"].unique())
 
 
 def load_team_season_stats(team_id, season):
     """Loads team level stats for a team/season."""
-    return _convert_and_fix_types(
-        _run_query("load_team_season_stats", {"team_id": team_id, "season": season})
+
+    if team_id not in available_teams:
+        raise Exception("Error: Team must be one of: " + str(available_teams))
+
+    if season not in available_seasons:
+        raise Exception("Error: Season must be one of: " + str(available_seasons))
+
+    return _fix_types(
+        pd.read_csv(
+            "https://github.com/nwslR/nwsldata/releases/download/team_season_summaries/{0}_{1}.csv".format(
+                team_id, season
+            ),
+            index_col=0,
+        )
     )
 
 
 def load_player_season_stats(team_id, season):
     """Loads player level stats for a team/season."""
-    return _convert_and_fix_types(
-        _run_query("load_player_season_stats", {"team_id": team_id, "season": season})
+
+    if team_id not in available_teams:
+        raise Exception("Error: Team must be one of: " + str(available_teams))
+
+    if season not in available_seasons:
+        raise Exception("Error: Season must be one of: " + str(available_seasons))
+
+    return _fix_types(
+        pd.read_csv(
+            "https://github.com/nwslR/nwsldata/releases/download/player_season_summaries/{0}_{1}.csv".format(
+                team_id, season
+            ),
+            index_col=0,
+        )
     )
 
 
 def load_team_match_stats(match_id):
     """Loads team level stats for a given match."""
-    return _convert_and_fix_types(
-        _run_query("load_team_match_stats", {"match_id": match_id})
+    matches = load_matches()
+    if match_id not in matches.index:
+        raise Exception(
+            "Error: Match does not exist! Please make sure you are using the correct Match ID."
+        )
+
+    return _fix_types(
+        pd.read_csv(
+            "https://github.com/nwslR/nwsldata/releases/download/team_match_summaries/{0}.csv".format(
+                match_id
+            ),
+            index_col=0,
+        )
     )
 
 
 def load_player_match_stats(match_id):
     """Loads player level stats for a given match."""
-    return _convert_and_fix_types(
-        _run_query("load_player_match_stats", {"match_id": match_id})
+    matches = load_matches()
+    if match_id not in matches.index:
+        raise Exception(
+            "Error: Match does not exist! Please make sure you are using the correct Match ID."
+        )
+
+    return _fix_types(
+        pd.read_csv(
+            "https://github.com/nwslR/nwsldata/releases/download/player_match_summaries/{0}.csv".format(
+                match_id
+            ),
+            index_col=0,
+        )
     )
--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from distutils.core import setup
 
 setup(name='nwslpy',
-      version='1.0',
+      version='0.0.0.4',
       description='Python wrapper around the nwslR library',
       packages=['nwslpy'],
       requires=[
-          "rpy2",
           "pandas",
       ])
