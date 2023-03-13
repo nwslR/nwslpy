fd8344bb234baafbf8ef4b6fc1f61936790daee6
Alison Gale <agale753@gmail.com>
2023-03-12 18:07:23 -0700

    Add extra zeros

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='nwslpy',
-      version='0.0.0.6',
+      version='0.0.0.6000',
       description='Python wrapper around the nwslR library',
       packages=['nwslpy'],
       requires=[
